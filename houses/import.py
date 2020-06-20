cf58e97de3424571abf8731d0b3e3d7474e731e3
Sahil <sahilflash123@gmail.com>
2020-06-20 11:21:46 +0530

    completed both import and roster

--- houses/import.py
+++ houses/import.py
@@ -1,1 +1,29 @@
 # TODO
+from sys import argv
+import csv
+from cs50 import SQL
+
+db = SQL("sqlite:///students.db")
+#function for reading with all condition
+def enter(csv_read_var):
+    fist_exception=0
+    for i in csv_read_var:
+        if fist_exception==0:
+            fist_exception+=1
+            continue
+        if len(i[0].split())<3:
+            name=i[0].split()
+            #print("if name " , name)
+            db.execute("INSERT INTO students (first , middle , last , house , birth) VALUES(? , ? , ? , ? , ?)",name[0],"NONE",name[1],i[1],i[2])
+        else:
+            name=i[0].split()
+            #print("else name" , name)
+            db.execute("INSERT INTO students (first , middle , last , house , birth) VALUES(? , ? , ? , ? , ?)",name[0],name[1],name[2],i[1],i[2])
+
+#reading from csv using function
+with open(argv[1],'r') as csv_file:
+    csv_read  = csv.reader(csv_file)
+    enter(csv_read)
+
+
+
--- houses/roster.py
+++ houses/roster.py
@@ -1,1 +1,19 @@
 # TODO
+from sys import argv
+import csv
+from cs50 import SQL
+
+db=SQL("sqlite:///students.db")
+
+#function for displaying clean data
+def out(house):
+    ok=db.execute("SELECT * FROM STUDENTS WHERE house = ?", house)
+    for i in ok:
+        print(i['first'],end=" ")
+        if(i['middle']!='NONE'):
+            print(i['middle'],end=" ")
+        print(i['last'],end=", ")
+        print("born",i['birth'])
+            
+
+out(argv[1])
